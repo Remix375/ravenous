f76716110340bc3a57a8581cef1e0bd134da3403
Remi Guillou <beaugossremi@gmail.com>
2021-04-18 15:29:02 +0200

    start + connecting files

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import logo from '../../logo.svg';
+import React from "react"
+import './App.css';
+import Business from "../Business/Business"
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Business/>
+    )
+  }
+}
+
+export default App;
